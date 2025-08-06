e87b219c7a263d886a3b06307979cfe0d4de2d21
池田　昌代 <icloud201312@icloud.com>
2025-08-06 20:44:35 +0900

    なんとなくトップのPC

--- _assets/front-page.php
+++ _assets/front-page.php
@@ -124,20 +124,25 @@
 
   <!-- トップ　登録制度について-->
   <section class="p-top-consider">
-    <h2>aaa</h2>
-    <!-- <div class="p-top-product__inner">
-      <div class="p-top-product__warp">
-        <div class="p-top-product__title-en c-ttl-bordering c-ttl-bordering--2row">
-          PRODUCT<br class="u-hidden-sp">
-          INTRODUCTION
-        </div>
-        <h2 class="p-top-product__title-ja c-text-20">製品紹介</h2>
-        <p class="p-top-product__text">
-          電気的エネルギーを機械的な動作に変換する装置を”ソレノイド”と呼んでいます。信和工業は”ソレノイド”を活用した流体制御機器で皆様のお役に立ち豊かな暮らしに貢献いたします。
-        </p>
-      </div>
+    <div class="c-inner">
+      <div class="p-top-consider__inner">
+        <h2 class="p-top-consider__title">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-consider.png" alt="">
+          <span>総合型地域スポーツクラブ</span>
+          <span>登録制度について</span>
 
-    </div> -->
+        </h2>
+        <p class="p-top-consider__text">
+          本制度は、国の第２期スポーツ基本計画に則り、日本スポーツ協会及び都道府県体育・スポーツ協会が関係機関と連携いて策定した制度です。本制度では、「総合型地域スポーツクラブ」の登録基準が示されており、すべての基準を満たしたクラブを「登録クラブ」として認定しています。<br>
+          地域住民や行政からの信頼性の向上やクラブ運営に対する透明性の確保を図ることで、地域に根ざしたクラブづくりを推進していきます。
+        </p>
+        <div class="p-top-consider__btn c-btn">
+          <a href="<?php echo esc_url(home_url('/')); ?>consider">
+            詳しくはこちら
+          </a>
+        </div>
+      </div><!-- /.p-top-consider__inner -->
+    </div><!-- /.c-inner -->
   </section>
 
 
@@ -146,16 +151,49 @@
 
   <!-- トップ　アクセス -->
   <section class="p-top-access">
-
+    <h2 class="c-ttl-img">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-access.png" alt="">
+      アクセス
+      <span>Access</span>
+    </h2>
+    <div class="p-top-access__map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d699.3542560957293!2d135.19921661966436!3d34.68849091750406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008ef641e566d7%3A0x9721a18edd6ed3fb!2z56We5oi45ZWG5bel6LK_5piT44K744Oz44K_44O844OT44Or!5e0!3m2!1sja!2sjp!4v1754478704911!5m2!1sja!2sjp"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div><!-- /.p-top-access__map -->
   </section>
   <!-- トップ　リンク -->
   <section class="p-top-link">
-    <ul class="p-top-link__items">
-
-      <li class="p-top-link__item">
-
-      </li>
-    </ul>
+    <div class="c-inner">
+      <ul class="p-top-link__items">
+        <?php
+        $club_page = get_page_by_path('club');
+        // 子ページを取得するクエリ
+        $club_children = new WP_Query(array(
+          'post_type' => 'page',
+          'post_parent' => $club_page->ID,
+          'posts_per_page' => -1,
+          'order' => 'ASC',
+          'orderby' => 'menu_order',
+        ));
+        ?>
+        <?php
+        while ($club_children->have_posts()):
+          $club_children->the_post(); ?>
+          <li class="p-top-link__item c-btn-link" style="background:<?php the_field('cf-area-color'); ?>;">
+            <a href="<?php echo esc_url(get_permalink()); ?>" >
+              <h3>
+                <?php if (get_field('cf-corporation')): ?>
+                  <?php the_field('cf-corporation'); ?><br>
+                <?php endif; ?>
+                <?php the_field('cf-name'); ?>
+              </h3>
+            </a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    </div><!-- /.c-inner -->
   </section>
 
 
