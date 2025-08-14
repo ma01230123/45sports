11f505d6b5d63507b2476832a30ffb1f41e120c4
池田　昌代 <icloud201312@icloud.com>
2025-08-14 15:04:38 +0900

    ４０４ページ

--- _assets/404.php
+++ _assets/404.php
@@ -1,26 +1,30 @@
 <?php get_header(); ?>
-<div class="u-headerHight"></div>
-<!-- 固定ページタイトル部分 -->
-<div class="c-thumbnail">
-  <div class="c-thumbnail__img">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company.jpg" alt="">
-  </div>
-  <h1 class="c-thumbnail__title c-ttl-thumbnail">
-    Not Found
-  </h1>
-</div>
 <!-- 固定ページパンくずリスト -->
 <?php get_template_part('template/bread'); ?>
-<div style="height:6rem" aria-hidden="true" class="wp-block-spacer"></div>
-<div class="c-inner">
-  <div class="c-inner-700 u-tac">
-    申し訳ありません。お探しのページはありませんでした。
-    <div style="height:5rem" aria-hidden="true" class="wp-block-spacer"></div>
-    <div class="c-btn-red">
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>company-guide" class="u-m0auto">TOPに戻る</a>
-    </div>
+
+<!-- 固定ページタイトル部分 -->
+<div class="l-page">
+  <div class="c-inner">
+    <div class="l-page__title">
+      <h1 class="c-ttl c-ttl--30">
+      Not Found
+      </h1>
+    </div><!-- /.l-page__title -->
+    <div style="height:6rem" aria-hidden="true" class="wp-block-spacer"></div>
+    <div class="l-page__content">
+      <div class="c-inner">
+        <div class="c-inner-700 u-tac">
+          申し訳ありません。お探しのページはありませんでした。
+          <div style="height:5rem" aria-hidden="true" class="wp-block-spacer"></div>
+          <div class="c-btn">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="u-m0auto">TOPに戻る</a>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.l-page__content -->
   </div>
 </div>
+
 <div style="height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
 <div style="height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
 
